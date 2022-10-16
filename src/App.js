a898c47f18986fccfb90ded42c7a0608155e8ca4
vvsungho <vvsungho@gmail.com>
2022-10-16 09:29:46 +0900

    react props study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import MyName from './MyName'
+import MyName2 from './MyName2'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = '윤성호';
+    return (
+        <Fragment>
+          <div>
+            <h1>안녕하세요 {name}님</h1>
+          </div>
+          <div>{1 + 1 === 2 ? '맞다' : '틀리다'}</div>
+          <div>{1 + 1 === 2 && '맞다'}</div>
+          <div>
+            {(function () {
+              if (name === '윤성호') {
+                return '맞다';
+              } else {
+                return '틀리다';
+              }
+            })()}
+          </div>
+          <div>
+            {() => {
+              if (name === '윤성호') {
+                return '맞다';
+              } else {
+                return '틀리다';
+              }
+            }}
+          </div>
+          <br/>
+          ==========================
+          <br/>
+          <MyName name = "윤성호" />
+          <MyName2 name = "윤성호2" />
+        </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/MyName2.js
+++ src/MyName2.js
@@ -1,22 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class MyName extends Component {
-    // props default를 지정하는 방법1
-    static defaultProps = {
-        name: '기본이름'
-    }
-    render() {
-        return (
-            <div>
-                 안녕하세요. 제 이름은 <u>{this.props.name}</u> 입니다.
-            </div>
-        );
-    }
-}
+const MyName2 = ({ name }) => {
+    return (
+        <div>
+            안녕하세요! 제 이름은 { name } 입니다.
+        </div>
+    );
+};
 
-// props default를 지정하는 방법2
-MyName.defaultProps = {
+MyName2.defaultProps = {
     name: '기본이름'
-}
+};
 
-export default MyName;
+export default MyName2;
